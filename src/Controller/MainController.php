c069cafdfbbdb22bbf5df90740645c14e3edde02
PhilRegnier <phil.regnier@gmail.com>
2022-03-28 14:20:08 +0200

    Add initial set of files

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    #[Route('/', name: "main_accueil")]
+    public function accueil(): \Symfony\Component\HttpFoundation\Response
+    {
+        return $this->render('main/accueil.html.twig');
+    }
+    #[Route('/about_us', name: "main_aboutUs")]
+    public function aboutUs(): \Symfony\Component\HttpFoundation\Response
+    {
+        $str_json = file_get_contents('../data/team.json');
+        $membres = json_decode($str_json,true);
+
+        return $this->render('main/about_us.html.twig',
+            compact('membres'));
+    }
+
+}
