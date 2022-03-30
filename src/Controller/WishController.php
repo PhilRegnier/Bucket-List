2cc10ff3abc6988e11eeb68084b02efd724836f0
PhilRegnier <phil.regnier@gmail.com>
2022-03-30 17:46:33 +0200

    Add initial set of files

---
+++ src/Controller/WishController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/wish', name: 'wish')]
+class WishController extends AbstractController
+{
+    #[Route('/list', name: '_list')]
+    public function liste(
+        WishRepository $wishRepository
+    ): Response
+    {
+        $wishes = $wishRepository->findAll();
+        return $this->render(
+            'wish/list.html.twig',
+            compact("wishes")
+        );
+    }
+    #[Route('/detail{id}', name: '_detail', requirements: ['id' => '\d+'])]
+    public function detail(
+        Wish $wish
+    ): Response
+    {
+        return $this->render(
+            'wish/detail.html.twig',
+            compact("wish"));
+    }
+
+}
